56f8163319c7efec35b130f21e8a4415630af89d
Poiroux Martin <poirouxmartin@gmail.com>
2023-09-19 14:37:49 +0200

    eval bar update

--- opti_chess/gui.h
+++ opti_chess/gui.h
@@ -180,10 +180,10 @@ static int base_time_white = 180000;
 static int base_time_black = 180000;
 
 // Incrément (en ms)
-//static int base_time_increment_white = -50;
-//static int base_time_increment_black = -50;
-static int base_time_increment_white = 15000;
-static int base_time_increment_black = 15000;
+static int base_time_increment_white = -50;
+static int base_time_increment_black = -50;
+//static int base_time_increment_white = 2000;
+//static int base_time_increment_black = 2000;
 
 // Valeur des pièces pour l'affichage sur la GUI (rien/roi, pion, cavalier, fou, tour, dame)
 static const int piece_GUI_values[6] = { 0, 1, 3, 3, 5, 9 };
