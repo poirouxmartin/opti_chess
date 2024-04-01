50d62cee48944374870dc5c8217c039d860758e0
Poiroux Martin <poirouxmartin@gmail.com>
2024-04-01 19:53:15 +0200

    gui update: sort move variants by eval on nodes equality

--- opti_chess/exploration.cpp
+++ opti_chess/exploration.cpp
@@ -1,4 +1,4 @@
-#include "exploration.h"
+Ôªø#include "exploration.h"
 #include "useful_functions.h"
 
 // Constructeur avec un plateau, un indice et un coup
@@ -18,7 +18,7 @@ void Node::add_child(Node* child) {
 	return _children.size();
 }
 
-// Fonction qui renvoie le numÈro du fils associÈ au coup s'il existe, -1 sinon
+// Fonction qui renvoie le num√©ro du fils associ√© au coup s'il existe, -1 sinon
 [[nodiscard]] int Node::get_child_index(Move move) const {
 	for (int i = 0; i < _children.size(); i++) {
 		if (_children[i]->_move == move) {
@@ -29,7 +29,7 @@ void Node::add_child(Node* child) {
 	return -1;
 }
 
-// Fonction qui renvoie l'indice du premier coup qui n'a pas encore ÈtÈ ajoutÈ, -1 sinon
+// Fonction qui renvoie l'indice du premier coup qui n'a pas encore √©t√© ajout√©, -1 sinon
 [[nodiscard]] int Node::get_first_unexplored_move_index() {
 	for (int i = _latest_first_move_explored + 1; i < _board._got_moves; i++) {
 		if (get_child_index(_board._moves[i]) == -1) {
@@ -50,30 +50,30 @@ void Node::grogros_zero(Buffer buffer, Evaluator eval, float beta, float k_add, 
 	// Si c'est un nouveau noeud, on fait son initialisation
 	if (_new_node) {
 
-		// VÈrifie si la partie est finie
+		// V√©rifie si la partie est finie
 		_board.is_game_over(); // Calcule les coup possibles au passage
 		_new_node = false;
 
-		// Trie les coups si Áa n'est pas dÈj‡ fait (les trie de faÁon rapide)
+		// Trie les coups si √ßa n'est pas d√©j√† fait (les trie de fa√ßon rapide)
 		!_board._sorted_moves && _board.sort_moves();
 	}
 
 	// Si la partie est finie, on ne fait rien
 	if (_board._game_over_value) {
-		// TODO: on fait quoi l‡??
+		// TODO: on fait quoi l√†??
 
 
 		_nodes++;
 		return;
 	}
 
-	// VÈrifie que le buffer n'est pas plein (TODO) (et est bien initialisÈ aussi)
+	// V√©rifie que le buffer n'est pas plein (TODO) (et est bien initialis√© aussi)
 	
 
 	// Exploration des noeuds
 	while (nodes > 0) {
 
-		// S'il reste des coups ‡ explorer
+		// S'il reste des coups √† explorer
 		if (children_count() < _board._got_moves) {
 			explore_new_move(buffer, eval);
 		}
@@ -90,7 +90,7 @@ void Node::grogros_zero(Buffer buffer, Evaluator eval, float beta, float k_add, 
 
 // Fonction qui explore un nouveau coup
 void Node::explore_new_move(Buffer buffer, Evaluator eval) {
-	// On prend le premier coup non explorÈ
+	// On prend le premier coup non explor√©
 	const int move_index = get_first_unexplored_move_index();
 	Move move = _board._moves[move_index];
 
@@ -104,9 +104,9 @@ void Node::explore_new_move(Buffer buffer, Evaluator eval) {
 	// Evalue le plateau, en regardant si la partie est finie
 	new_board.evaluate(&eval, false, nullptr, true);
 
-	// Met ‡ jour l'Èvaluation du plateau
+	// Met √† jour l'√©valuation du plateau
 	if (children_count() == 0) {
-		_board._evaluation = new_board._evaluation; // Pour le quiescence search, faut-il ne pas aller l‡ pour garder un standpat?
+		_board._evaluation = new_board._evaluation; // Pour le quiescence search, faut-il ne pas aller l√† pour garder un standpat?
 	}
 	else {
 		_board._evaluation = _board._player * max(_board._evaluation, new_board._evaluation) + !_board._player * min(_board._evaluation, new_board._evaluation);
@@ -121,15 +121,15 @@ void Node::explore_new_move(Buffer buffer, Evaluator eval) {
 	add_child(child);	
 }
 
-// Fonction qui explore dans un plateau fils pseudo-alÈatoire
+// Fonction qui explore dans un plateau fils pseudo-al√©atoire
 void Node::explore_random_child(Buffer buffer, Evaluator eval, float beta, float k_add) {
-	// Prend un fils alÈatoire
+	// Prend un fils al√©atoire
 	int child_index = pick_random_child_index(beta, k_add);
 
 	// On explore ce fils
-	_children[child_index]->grogros_zero(buffer, eval, beta, k_add, 1); // L'Èvaluation du fils est mise ‡ jour ici
+	_children[child_index]->grogros_zero(buffer, eval, beta, k_add, 1); // L'√©valuation du fils est mise √† jour ici
 	
-	// Met ‡ jour l'Èvaluation du plateau
+	// Met √† jour l'√©valuation du plateau
 	if (_board._player) {
 		_board._evaluation = -INT_MAX;
 		for (int i = 0; i < children_count(); i++) {
@@ -148,7 +148,7 @@ void Node::explore_random_child(Buffer buffer, Evaluator eval, float beta, float
 	_nodes++;
 }
 
-// Fonction qui renvoie parmi une liste d'entiers, renvoie un index alÈatoire, avec une probabilitÈ variantes, en fonction de la grandeur du nombre correspondant ‡ cet index
+// Fonction qui renvoie parmi une liste d'entiers, renvoie un index al√©atoire, avec une probabilit√© variantes, en fonction de la grandeur du nombre correspondant √† cet index
 int Node::pick_random_child_index(const float beta, const float k_add) {
 	int color = _board.get_color();
 	int n_children = children_count();
@@ -160,18 +160,18 @@ int Node::pick_random_child_index(const float beta, const float k_add) {
 	for (int i = 0; i < n_children; i++)
 		l2[i] = color * _children[i]->_board._evaluation;
 
-	// Softmax sur les Èvaluations
+	// Softmax sur les √©valuations
 	softmax(l2, n_children, beta, k_add);
 
 
 	// *** 2. ***
-	// Liste de pondÈration en fonction de l'exploration de chaque noeud (donne plus de poids aux noeuds moins explorÈs)
+	// Liste de pond√©ration en fonction de l'exploration de chaque noeud (donne plus de poids aux noeuds moins explor√©s)
 	float pond[100];
 
 	for (int i = 0; i < n_children; i++)
 		pond[i] = static_cast<float>(_nodes) / static_cast<float>(_children[i]->_nodes);
 
-	// On applique la pondÈration aux Èvaluations
+	// On applique la pond√©ration aux √©valuations
 	nodes_weighting(l2, pond, n_children);
 
 	// *** 3. ***
@@ -183,7 +183,7 @@ int Node::pick_random_child_index(const float beta, const float k_add) {
 		sum += l2[i];
 	}
 
-	// Choix du coup en fonction d'une valeur alÈatoire
+	// Choix du coup en fonction d'une valeur al√©atoire
 	const int rand_val = rand_int(1, sum);
 	int cumul = 0;
 
@@ -197,19 +197,53 @@ int Node::pick_random_child_index(const float beta, const float k_add) {
 	return 0;
 }
 
-// Fonction qui renvoie le fils le plus explorÈ
-[[nodiscard]] int Node::get_most_explored_child_index() {
+// Fonction qui renvoie le fils le plus explor√©
+[[nodiscard]] int Node::get_most_explored_child_index(bool decide_by_eval) {
 	int max = 0;
-	int index = 0;
 
-	for (int i = 0; i < children_count(); i++) {
-		if (_children[i]->_nodes > max) {
-			max = _children[i]->_nodes;
-			index = i;
+	// Tri simple, on ne d√©partage pas les √©galit√©s
+	if (!decide_by_eval) {
+
+		int index = 0;
+
+		for (int i = 0; i < children_count(); i++) {
+			if (_children[i]->_nodes > max) {
+				max = _children[i]->_nodes;
+				index = i;
+			}
 		}
+
+		return index;
 	}
 
-	return index;
+	// Avec un d√©partage par √©galit√©
+	else {
+
+		vector<int> indices;
+		int color = _board.get_color();
+
+		for (int i = 0; i < children_count(); i++) {
+			if (_children[i]->_nodes >= max) {
+				max = _children[i]->_nodes;
+				indices.push_back(i);
+			}
+		}
+
+		// En cas d'√©galit√©, on trie par √©valuation
+		int max_eval = -INT_MAX;
+		int index = 0;
+
+		for (int i = 0; i < indices.size(); i++) {
+			if (_children[indices[i]]->_board._evaluation * color > max_eval) {
+				max_eval = _children[indices[i]]->_board._evaluation * color;
+				index = indices[i];
+			}
+		}
+
+		return index;
+	}
+
+	
 }
 
 // Reset le noeud et ses enfants, et les supprime tous
@@ -231,57 +265,87 @@ void Node::reset() {
 // Fonction qui renvoie les variantes d'exploration
 string Node::get_exploration_variants(bool main) {
 
+	// TODO: il faut rajouter les 1. .., 2...
+	// Pour les eval, il faut afficher diff√©remment les mats
+	// Afficher les ic√¥nes des pi√®ces (‚ôî, ‚ôï, ‚ôñ, ‚ôó, ‚ôò, ‚ôô, ‚ôö, ‚ôõ, ‚ôú, ‚ôù, ‚ôû, ‚ôü) (UTF-8)
+
 	// Si on est en fin de variante
 	if (_board._game_over_value) {
 		return "";
 	}
-	/*if (_board._game_over_value == -1) {
-		return "# 0-1";
-	}
-	if (_board._game_over_value == 1) {
-		return "# 1-0";
-	}
-	if (_board._game_over_value == 2) {
-		return "1/2-1/2";
-	}*/
 
 	string variants = "";
 
-	// S'il y a des coups explorÈs
+	// S'il y a des coups explor√©s
 	if (children_count() > 0) {
 
 		// Si on est dans le noeud principal, on affiche toutes les variantes
 		if (main) {
-			// Trie les enfants par nombre de noeuds (et sur une ÈgalitÈ, par Èvaluation - TODO)
+			// Trie les enfants par nombre de noeuds
 			vector<pair<int, int>> children_nodes;
 			for (int i = 0; i < children_count(); i++) {
-				children_nodes.push_back(make_pair(-_children[i]->_nodes, i));
+				children_nodes.push_back(make_pair(-_children[i]->_nodes, i)); // On met un moins pour trier dans l'ordre d√©croissant
 			}
 
 			sort(children_nodes.begin(), children_nodes.end());
 
+			// En cas d'√©galit√©, on trie par √©valuation
+			vector<int> children_index;
+
+			vector<pair<int, int>> children_evaluations;
+
+			int previous_nodes = children_nodes[0].first;
+			int color = _board.get_color();
+
+			children_evaluations.push_back(make_pair(- _children[children_nodes[0].second]->_board._evaluation * color, children_nodes[0].second));
+
+			for (int i = 1; i < children_count() + 1; i++) {
+
+				// Fin des √©galit√©s, on trie par √©valuation
+				if (i == children_count() || children_nodes[i].first != children_nodes[i - 1].first) {
+					sort(children_evaluations.begin(), children_evaluations.end());
+					for (int j = 0; j < children_evaluations.size(); j++) {
+						children_index.push_back(children_evaluations[j].second);
+					}
+
+					children_evaluations.clear();
+
+					// Enfant √† trier
+					if (i < children_count()) {
+						children_evaluations.push_back(make_pair(- _children[children_nodes[i].second]->_board._evaluation * color, children_nodes[i].second));
+					}
+				}
+				else {
+					children_evaluations.push_back(make_pair(- _children[children_nodes[i].second]->_board._evaluation * color, children_nodes[i].second));
+				}
+
+				
+			}
+
+
 			for (int i = 0; i < children_count(); i++) {
-				variants += _board.move_label(_children[children_nodes[i].second]->_move) + " " + _children[children_nodes[i].second]->get_exploration_variants(false);
-				// TODO: il faut rajouter les 1. .., 2...
+				variants += _board.move_label(_children[children_index[i]]->_move) + " " + _children[children_index[i]]->get_exploration_variants(false);
 
 				// Nombre de noeuds...
-				variants += "Eval: " + int_to_round_string(_children[children_nodes[i].second]->_board._evaluation) + " | Nodes: " + int_to_round_string(_children[children_nodes[i].second]->_nodes) + "\n\n";
+				variants += "\nEval: " + int_to_round_string(_children[children_index[i]]->_board._evaluation) + " | Nodes: " + int_to_round_string(_children[children_index[i]]->_nodes) + "\n\n";
 			}
 		}
 
-		// Sinon, on affiche seulement le coup le plus explorÈ
+		// Sinon, on affiche seulement le coup le plus explor√©
 		else {
-			// Affiche seulement le premier coup (le plus explorÈ)
-			variants += _board.move_label(_children[get_most_explored_child_index()]->_move) + " " + _children[get_most_explored_child_index()]->get_exploration_variants(false);
+			// Affiche seulement le premier coup (le plus explor√©, et en cas d'√©galit√©, celui avec la meilleure √©valuation)
+			int index_best_move = get_most_explored_child_index();
+
+			variants += _board.move_label(_children[index_best_move]->_move) + " " + _children[index_best_move]->get_exploration_variants(false);
 		}
 		
 	}
 
-	// S'il n'y a pas de coups explorÈs
+	// S'il n'y a pas de coups explor√©s
 	else {
-		variants = "(" + int_to_round_string(_board._evaluation) + ")\n";
 
-		// TODO: on peut afficher l'Èvaluation du plateau, le nombre de noeuds, le temps de calcul...
+		// On affiche l'√©valuation du plateau en fin de variante
+		variants = "(" + int_to_round_string(_board._evaluation) + ")";
 	}
 	
 	return variants;
