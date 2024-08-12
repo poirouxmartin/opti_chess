2b35225289d140f218f38470670124145bebd5b9
Poiroux Martin <poirouxmartin@gmail.com>
2024-08-12 23:18:58 +0200

    fix passed pawns

--- opti_chess/exploration.h
+++ opti_chess/exploration.h
@@ -35,6 +35,9 @@ public:
 	int _nodes = 0;
 	//int _nodes = count_children_nodes() + 1;
 
+	// TODO: il faut plusieurs types de noeuds: noeuds quiet (ceux recherchés par GrogrosZero), noeuds de quiescence, noeuds de transposition...
+	// On utilisera les research_nodes pour le temps de calcul de Grogros, l'affichage des flèches etc...
+
 	// Temps de calcul
 	clock_t _time_spent = 0;
 
