802dc4652fbfa6bc6888a8a2931ef23570e2c406
Poiroux Martin <poirouxmartin@gmail.com>
2024-10-11 14:32:48 +0200

    updates

--- opti_chess/exploration.cpp
+++ opti_chess/exploration.cpp
@@ -379,7 +379,8 @@ Move Node::pick_random_child(const float beta, const float k_add) const {
 
 	// Faut-il chercher plus large en finale?
 	//double enlargement_factor = (double)_nodes / (double)_iterations;
-	double enlargement_factor = pow((double)_nodes / (double)_iterations / (1 - _board->_adv + 0.5f), 0.3);
+	double enlargement_factor = pow((double)_nodes / (double)_iterations / (1 - _board->_adv + 0.5f), 0.2);
+	//double enlargement_factor = 1;
 	
 	// FIXME: quelle est la meilleure manière d'élargir?
 	// Réduire beta? Augmenter k_add? Les deux?
