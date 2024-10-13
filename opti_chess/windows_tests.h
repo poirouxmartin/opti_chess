e641f11fb85744f6bcd0be39752fc2df6e4e5de9
Poiroux Martin <poirouxmartin@gmail.com>
2024-10-14 01:08:49 +0200

    king update (not so good?) + add new website binding + fix repetition bug

--- opti_chess/windows_tests.h
+++ opti_chess/windows_tests.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <string>
+#include <utility>
+
+using namespace std;
+
 // Color
 class SimpleColor {
 public:
@@ -22,6 +27,48 @@ typedef struct SimpleRectangle {
 	int y2;
 };
 
+// Site de jeux d'échecs
+class ChessSite {
+public:
+
+	// Nom du site
+	string _name;
+
+	// Couleur des cases blanches
+	SimpleColor _white_tile_color;
+
+	// Couleur des cases noires
+	SimpleColor _black_tile_color;
+
+	// Couleur des pièces blanches
+	SimpleColor _white_piece_color;
+
+	// Couleur des pièces noires
+	SimpleColor _black_piece_color;
+
+	// Couleur d'une case blanche jouée
+	SimpleColor _white_tile_played_color;
+
+	// Couleur d'une case noire jouée
+	SimpleColor _black_tile_played_color;
+
+	// A quel endroit regarder sur la case pour être sûr de trouver la couleur de la pièce (en % de la taille de la case, et en partant du coin bas-gauche)
+	pair<float, float> _piece_location_on_tile;
+
+	// A quel endroit regarder sur la case pour être sûr de trouver la couleur de la case (en % de la taille de la case, et en partant du coin bas-gauche)
+	pair<float, float> _tile_location_on_tile;
+
+	// Temps perdu par coup
+	int _time_lost_per_move;
+
+
+	// Constructeur
+
+	// Constructeur par défaut
+	ChessSite();
+};
+
+
 // Fonction qui simule un clic de souris à une position donnée
 void simulate_mouse_click(int x, int y);
 
@@ -35,13 +82,13 @@ void set_mouse_pos(int x, int y);
 unsigned long long  get_total_system_memory();
 
 // Fonction qui affiche la couleur de chacune des cases de l'échiquier sur l'écran, en donnant ses coordonnées (top-left, bottom-right)
-uint_fast8_t* get_board_move(int x1, int y1, int x2, int y2, bool orientation = false, bool display = false);
+uint_fast8_t* get_board_move(int x1, int y1, int x2, int y2, ChessSite website, bool orientation = false, bool display = false);
 
 // Fonction qui clique un coup en fonction de l'orientation du plateau
 void click_move(int j1, int i1, int j2, int i2, int x1, int y1, int x2, int y2, bool orientation = false);
 
 // Fonction qui récupère l'orientation du plateau. Renvoie 1 si les blancs sont en bas, 0 si c'est les noirs, -1 sinon
-int bind_board_orientation(int x1, int y1, int x2, int y2);
+int bind_board_orientation(int x1, int y1, int x2, int y2, ChessSite website);
 
 // Fonction qui cherche la position du plateau de chess.com sur l'écran
-void locate_chessboard(int&, int&, int&, int&);
+bool locate_chessboard(int&, int&, int&, int&, ChessSite website);
