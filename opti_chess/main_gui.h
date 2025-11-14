049f044e28bae9dfee8dea4d5e6cc291fa42aa0e
poirouxmartin <poirouxmartin@gmail.com>
2025-11-14 17:23:21 +0100

    first bitboard update implementation

--- opti_chess/main_gui.h
+++ opti_chess/main_gui.h
@@ -123,6 +123,7 @@ inline int main_ui() {
 	Board test_board;
 	//test_board.from_fen("rnbqkbnr/pppp1ppp/8/4p3/6P1/5P2/PPPPP2P/RNBQKBNR b KQkq - 0 2");
 	main_GUI._root_exploration_node = new Node(&test_board); // FIXME: à faire autre part (dans la GUI?)
+	main_GUI.reset_game();
 
 
 	// Test de réseaux de neurones
@@ -164,8 +165,9 @@ inline int main_ui() {
 			//print_controls(controls_around_king);
 
 			// Position test pour la reflexion en milieu de partie: r1bq1b1r/pp4pp/2p1k3/3np3/1nBP4/2N2Q2/PPP2PPP/R1B2RK1 b - - 0 10
-
-			main_GUI._board.validate_nodes_count_at_depth("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 6, { 1, 20, 400, 8902, 197281, 4865609, 119060324, 3195901860 }, true);
+			
+			main_GUI._board.benchmark_nodes_count_at_depth("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 6, { 1, 20, 400, 8902, 197281, 4865609, 119060324, 3195901860 }, 10, true);
+			//main_GUI._board.validate_nodes_count_at_depth("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 6, { 1, 20, 400, 8902, 197281, 4865609, 119060324, 3195901860 }, true);
 			//main_GUI._board.validate_nodes_count_at_depth("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 7, { 1, 20, 400, 8902, 197281, 4865609, 119060324, 3195901860 }, true, false, true);
 			//main_GUI._board.validate_nodes_count_at_depth("rnbqkbnr/pppppppp/8/8/8/3P4/PPP1PPPP/RNBQKBNR b KQkq - 0 1", 6, { 1, 20, 539, 11959, 328511, 8073082, 227598692 }, true, true);
 			//main_GUI._board.validate_nodes_count_at_depth("rnbqkbnr/pp1ppppp/8/2p5/8/3P4/PPP1PPPP/RNBQKBNR w KQkq - 0 2", 3, { 1, 27, 593, 15971 }, true, true);
@@ -235,6 +237,12 @@ inline int main_ui() {
 
 			//main_GUI._board.update_bitboards();
 			//main_GUI._board.print_all_bitboards();
+		}
+
+		// B - Bitboards
+		if (IsKeyPressed(KEY_B)) {
+			//main_GUI._board.update_bitboards();
+			main_GUI._board.print_all_bitboards();
 		}
 
 		// Q - Quiescence
